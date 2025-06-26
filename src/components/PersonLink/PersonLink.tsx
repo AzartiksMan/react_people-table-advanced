422bf363019e040e3b66422aedb6ffea7796442b
AzartiksMan <azartofgod@gmail.com>
2025-06-26 18:52:21 +0300

    Fix smth

--- src/components/PersonLink/PersonLink.tsx
+++ src/components/PersonLink/PersonLink.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import cn from 'classnames';
 
 interface Props {
@@ -9,12 +9,14 @@ interface Props {
 }
 
 export const PersonLink: React.FC<Props> = ({ name, slug, isFemale }) => {
+  const location = useLocation();
+
   return (
     <Link
       className={cn({
         'has-text-danger': isFemale,
       })}
-      to={`/people/${slug}`}
+      to={`/people/${slug}${location.search}`}
     >
       {name}
     </Link>
